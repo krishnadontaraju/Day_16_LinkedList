ef9bf16b5006e3f99f26b0336c320da8cb094c30
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-04-11 16:35:00 +0530

    ADDED : test to validate pop method

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,9 +1,28 @@
-
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LinkedListTest {
+
+    /**
+     * Testing the pop method which should remove the first element
+     */
+    @Test
+    void whenPopped_ShouldRemove_TheLastElement() {
+        NodeClass<Integer> firstNode = new NodeClass<>(56);
+        NodeClass<Integer> secondNode = new NodeClass<>(30);
+        NodeClass<Integer> thirdNode = new NodeClass<>(70);
+
+        CustomLinkedList customLinkedList = new CustomLinkedList();
+
+        customLinkedList.add(thirdNode);
+        customLinkedList.add(secondNode);
+        customLinkedList.add(firstNode);
+
+        customLinkedList.pop();
+
+        Assertions.assertTrue(customLinkedList.head.equals(secondNode) &&
+                customLinkedList.tail.equals(thirdNode));
+    }
 
     /**
      * Testing the Insert method to return the list as expected
@@ -19,11 +38,11 @@ public class LinkedListTest {
 
         customLinkedList.add(firstNode);
         customLinkedList.append(thirdNode);
-        customLinkedList.insertNodeBetween(firstNode,secondNode);
+        customLinkedList.insertNodeBetween(firstNode, secondNode);
 
         Assertions.assertTrue(customLinkedList.head.equals(firstNode) &&
-                                        customLinkedList.head.getNextNode().equals(secondNode) &&
-                                        customLinkedList.tail.equals(thirdNode) );
+                customLinkedList.head.getNextNode().equals(secondNode) &&
+                customLinkedList.tail.equals(thirdNode));
     }
 
     /**
@@ -42,8 +61,8 @@ public class LinkedListTest {
         customLinkedList.add(thirdNode);
 
         Assertions.assertTrue(customLinkedList.head.equals(thirdNode) &&
-                                        customLinkedList.head.getNextNode().equals(secondNode) &&
-                                        customLinkedList.tail.equals(firstNode) );
+                customLinkedList.head.getNextNode().equals(secondNode) &&
+                customLinkedList.tail.equals(firstNode));
     }
 
     /**
@@ -63,8 +82,8 @@ public class LinkedListTest {
         customLinkedList.append(thirdNode);
 
         Assertions.assertTrue(customLinkedList.tail.equals(thirdNode) &&
-                                        customLinkedList.tail.getNextNode().equals(secondNode) &&
-                                        customLinkedList.head.equals(firstNode) );
+                customLinkedList.tail.getNextNode().equals(secondNode) &&
+                customLinkedList.head.equals(firstNode));
     }
 
     /**
