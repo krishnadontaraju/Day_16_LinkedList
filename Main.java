ceead67e51b3959976a87810f98446df9e6515f6
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-04-11 19:04:11 +0530

    ADDED : search and insert method

--- Main.java
+++ Main.java
@@ -3,5 +3,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("THIS IS THE LINKED LIST OPERATION TEST, WELCOME");
+
+        NodeClass<Integer> firstNode = new NodeClass<>(56);
+        NodeClass<Integer> secondNode = new NodeClass<>(30);
+        NodeClass<Integer> thirdNode = new NodeClass<>(70);
+        NodeClass<Integer> fourthNode = new NodeClass<>(40);
+
+        CustomLinkedList customLinkedList = new CustomLinkedList();
+
+        customLinkedList.append(firstNode);
+        customLinkedList.append(secondNode);
+        customLinkedList.append(thirdNode);
+
+        customLinkedList.insertNodeAfterBySearching(30,fourthNode);
+
+        customLinkedList.printAllNodes();
     }
 }
